f8efd922a853159b475b77ae21d60057c3939612
Vincent Dahmen <4dahmen@informatik.uni-hamburg.de>
2015-11-23 20:09:46 +0100

    Aufgabe 4 teillösung hinzugefügt

--- 05/blatt5.pl
+++ 05/blatt5.pl
@@ -348,6 +348,18 @@ stromaufwaerts(oder,szczecin,swinoujscie,61,8).
 
 
 
+% Aufgabe 4
+
+% Rekursive Definition für eine Suche
+
+ist_erreichbar_von(DirA, DirB):- directory(DirA,_,DirB,_,_).
+ist_erreichbar_von(DirA, DirB):- ist_erreichbar_von(DirB, DirA).
+ist_erreichbar_von(DirA, DirB):- directory(DirA,_,DirC,_,_), ist_erreichbar_von(DirC, DirB).
+
+ist_erreichbar_von_root(FileId):- file(FileId,Dir,_,_,_,_), ist_erreichbar_von(Dir, 0).
+
+ist_unterverzeichnis_von(DirA, DirB):- directory(DirA,_,DirB,_,_).
+ist_unterverzeichnis_von(DirA, DirB):- directory(DirC,_,DirA,_,_), ist_unterverzeichnis_von(DirB DirC).
 
 
 
